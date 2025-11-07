72cb52c0492afcab1213928bcb57df2c00431498
hcsarker <hcsarker2002@gmail.com>
2025-11-07 17:42:13 +0600

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -11,15 +11,8 @@ export default defineConfig(({ mode }) => ({
   plugins: [react()],
 
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        }
-      }
-    }
+
+
   },
 
   resolve: {
